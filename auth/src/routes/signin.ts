4beadb5242f5ec8ba6cf4305a57be28fb76ec434
riyaz <riyazpasha880@gmail.com>
2021-03-25 23:44:24 +0530

    check is user already signedup or not if already signed up then check whether is provided validpassword and then return jwt token on login

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors/bad-request-error";
 import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
@@ -15,9 +19,29 @@ router.post(
             .withMessage("Please provide password")
     ],
     validateRequest,
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequestError("Invalid Credentials");
+        }
 
-        res.send("Hi There.. signin successful");
+        const passwordsMatch = await Password.comapare(existingUser.password, password);
+        if (!passwordsMatch) {
+            throw new BadRequestError("Invalid Credentials");
+        }
+
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email,
+        }, process.env.JWT_KEY!
+        );
+
+        req.session = {
+            jwt: userJwt,
+        }
+
+        res.status(200).send(existingUser);
     });
 
 export {
