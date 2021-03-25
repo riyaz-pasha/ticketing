134ee0d2026769312449236a053a508c18a6e6aa
riyaz <riyazpasha880@gmail.com>
2021-03-25 22:46:29 +0530

    check whether jwt key is available in environment variable or not during auth service start itself if not throw an Error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -37,7 +37,8 @@ router.post("/api/users/signup", [
     const userJwt = jwt.sign({
         id: user.id,
         email: user.email,
-    }, "asdf");
+    }, process.env.JWT_KEY!
+    );
 
     req.session = {
         jwt: userJwt,
