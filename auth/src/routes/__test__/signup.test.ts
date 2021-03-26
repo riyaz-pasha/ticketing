6db34bea2211141fd5bd40aeaf22ad0ad3da3466
riyaz <riyazpasha880@gmail.com>
2021-03-27 00:16:51 +0530

    added error case scenario tests for signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,6 +1,10 @@
 import request from "supertest";
 import { app } from "../../app";
 
+const signupRequest = (reqBody) => request(app)
+    .post("/api/users/signup")
+    .send(reqBody)
+
 it('should return a 201 response on succcessful signup', async () => {
     return request(app)
         .post("/api/users/signup")
@@ -9,4 +13,35 @@ it('should return a 201 response on succcessful signup', async () => {
             password: "password",
         })
         .expect(201)
+});
+
+it('should return a 400 when in valid email is passed', async () => {
+    return signupRequest({
+        email: "test",
+        password: "password",
+    }).expect(400)
+});
+
+it('should return a 400 when in valid password is passed', async () => {
+    return signupRequest({
+        email: "test@test.com",
+        password: "p",
+    }).expect(400)
+});
+
+it('should return a 400 when email & password are missing', async () => {
+    return signupRequest({})
+        .expect(400)
+});
+
+it('should return a 400 when email is missing', async () => {
+    return signupRequest({
+        password: "password",
+    }).expect(400)
+});
+
+it('should return a 400 when password is missing', async () => {
+    return signupRequest({
+        email: "test@test.com",
+    }).expect(400)
 });
