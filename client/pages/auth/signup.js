03f9322a0d2d43ceb0754c96c03d321292bc0b89
riyaz <riyazpasha880@gmail.com>
2021-03-30 22:40:42 +0530

    create useRequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,23 +1,20 @@
 import { useState } from 'react';
 import axios from "axios";
+import useRequest from '../../hooks/useRequest';
 
 function Signup() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState([]);
+    const { errors, doRequest } = useRequest({
+        url: "/api/users/signup",
+        method: "post",
+        body: { email, password }
+    });
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        try {
-            const response = await axios.post("/api/users/signup", {
-                email: email,
-                password: password,
-            })
-            console.log("response", response.data);
-        } catch (error) {
-            setErrors(error.response.data.errors)
-        }
+        doRequest();
     }
 
     return (
@@ -40,14 +37,7 @@ function Signup() {
                     className="form-control"
                 />
             </div>
-            {
-                errors.length > 0 && <div className="alert alert-danger">
-                    <h4>Oops...</h4>
-                    <ul className="my-0">
-                        {errors.map(error => <li key={error.message}>{error.message}</li>)}
-                    </ul>
-                </div>
-            }
+            {errors}
             <button className="btn btn-primary">Sign up</button>
         </form>
     )
