b9a60abff15820638a22551bf2cc31deb1604438
riyaz <riyazpasha880@gmail.com>
2021-03-31 22:25:49 +0530

    created sign in page

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import Router from "next/router";
+import useRequest from '../../hooks/useRequest';
+
+const Signin = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { errors, doRequest } = useRequest({
+        url: "/api/users/signin",
+        method: "post",
+        body: { email, password },
+        onSuccess: () => Router.push("/")
+    });
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        doRequest();
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h1>Sign In</h1>
+            <div className="form-group">
+                <label>Email</label>
+                <input
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    className="form-control"
+                />
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    className="form-control"
+                />
+            </div>
+            {errors}
+            <button className="btn btn-primary">Sign In</button>
+        </form>
+    )
+}
+
+export default Signin
