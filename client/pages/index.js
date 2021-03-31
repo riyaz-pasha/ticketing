cfd1686de35327dff87faeeb98c97953f9f28bde
riyaz <riyazpasha880@gmail.com>
2021-03-31 22:13:00 +0530

    extracted common client check logic to buildClient utility function

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import buildClient from "../api/buildClient"
 
 const LandingPage = ({ currentUser }) => {
     console.log("Comp ", currentUser)
@@ -6,20 +7,10 @@ const LandingPage = ({ currentUser }) => {
     return <h1>Landing Page</h1>
 }
 
-LandingPage.getInitialProps = async ({ req }) => {
-    if (typeof window === "undefined") {
-        const { data } = await axios.get(
-            "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-            {
-                headers: req.headers,
-            }
-        );
-        return data;
-    } else {
-        const { data } = await axios.get("/api/users/currentuser");
-        return data;
-    }
-
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context)
+    const { data } = await client.get("/api/users/currentuser");
+    return data;
 }
 
 export default LandingPage;
