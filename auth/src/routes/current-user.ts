17b407cc6b9529403a5620412474487ecccf350f
riyaz <riyazpasha880@gmail.com>
2021-04-01 23:27:03 +0530

    use errors and middlewares from common npm package in auth service

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
+import { currentUser } from "@riyazpasha/ticketing-common";
 import express from "express";
-import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
