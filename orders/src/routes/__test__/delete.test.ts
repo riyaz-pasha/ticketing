3a48ce8bb63857fc5cf6b46f745ccadfc3be4cb5
riyaz <riyazpasha880@gmail.com>
2021-04-10 02:22:15 +0530

    update status to cancelled whenever user cancells an order

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,44 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Order, OrderStatus } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+const deleteOrderById = (orderId: string, userCookie: string[]) => request(app)
+    .delete(`/api/orders/${orderId}`)
+    .set("Cookie", userCookie)
+    .send()
+
+const makeOrder = (ticketId: string, userCookie?: string[]) => request(app)
+    .post("/api/orders")
+    .set("Cookie", userCookie || global.signin())
+    .send({ ticketId });
+
+const buildTicket = async () => {
+    const ticket = Ticket.build({ title: "Movie Name", price: 100 });
+    await ticket.save();
+    return ticket;
+}
+
+it('should change the status of order to cancelled', async () => {
+    const ticket1 = await buildTicket();
+    const user1 = global.signin();
+    const { body: user1Order1 } = await makeOrder(ticket1.id, user1);
+
+    const response = await deleteOrderById(user1Order1.id, user1);
+
+    expect(response.status).toEqual(204);
+    const updatedOrder = await Order.findById(user1Order1.id);
+    expect(updatedOrder?.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('should not cancel other user order details', async () => {
+    const ticket1 = await buildTicket();
+    const user1 = global.signin();
+    const user2 = global.signin();
+    const { body: user1Order1 } = await makeOrder(ticket1.id, user1);
+
+    const response = await deleteOrderById(user1Order1.id, user2);
+
+    expect(response.status).toEqual(401);
+    expect(response.body.errors[0].message).toEqual("Not authorized");
+});
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,10 +1,22 @@
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@riyazpasha/ticketing-common';
 import { Request, Response, Router } from "express";
+import { Order, OrderStatus } from '../models/order';
 
 const router = Router();
 
 router.delete(
     "/api/orders/:orderId",
+    requireAuth,
     async (req: Request, res: Response) => {
+        const { orderId } = req.params;
+        const order = await Order.findById(orderId);
+        if (!order) throw new NotFoundError();
+        if (order.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+
+        order.status = OrderStatus.Cancelled;
+        await order.save();
+
+        res.status(204).send(order);
     }
 )
 
