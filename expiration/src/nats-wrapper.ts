aea8a7093ec43a50091f84e5da96b8f73a66f403
riyaz <riyazpasha880@gmail.com>
2021-04-11 00:18:11 +0530

    added base expiration setup

---
+++ expiration/src/nats-wrapper.ts
@@ -0,0 +1,31 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class NatsWrapper {
+    private _client?: Stan;
+
+    get client() {
+        if (!this._client) throw new Error("Cannot access NATS client before connecting");
+        return this._client;
+    }
+
+    connect(clusterId: string, clientId: string, url: string): Promise<void> {
+        this._client = nats.connect(clusterId, clientId, { url });
+
+        return new Promise((res, rej) => {
+            this.client.on("connect", () => {
+                console.log("🚀 NATS connected");
+                res();
+            });
+            this.client.on("error", (err) => {
+                console.error("🚀 error connecting to NATS");
+                rej(err);
+            })
+        })
+    }
+}
+
+const natsWrapper = new NatsWrapper()
+
+export {
+    natsWrapper,
+};
