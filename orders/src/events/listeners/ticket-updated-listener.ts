0efe23fbe3909fa89aa7d1093e107ea6afa40d92
riyaz <riyazpasha880@gmail.com>
2021-04-10 15:38:27 +0530

    optimistic version cotrol in ticket db.. update ticket version on save .

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,7 +8,7 @@ class TicketUpdatedListener extends Listener<TicketUpdatedEvent>{
     queueGroupName: string = queueGroupName;
 
     async onMessage(data: TicketUpdatedEvent["data"], msg: Message): Promise<void> {
-        const ticket = await Ticket.findById(data.id);
+        const ticket = await Ticket.findByEvent(data);
         if (!ticket) throw new Error("Ticket not found");
 
         const { title, price } = data;
