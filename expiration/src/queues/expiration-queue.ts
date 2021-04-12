1fbfb27e69c10b3d921bc3a316b8f9efe3bf073a
riyaz <riyazpasha880@gmail.com>
2021-04-12 10:26:36 +0530

    publish expiration complete event once order is expired

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -1,4 +1,6 @@
 import Queue from "bull";
+import { ExpirationCompletePublisher } from "../events/publishers/expiration-complete-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 interface Payload {
     orderId: string
@@ -11,7 +13,9 @@ const expirationQueue = new Queue<Payload>("order:expiration", {
 });
 
 expirationQueue.process(async function (job) {
-    console.log("Publish order expiration event for orderId: ", job.data.orderId);
+    new ExpirationCompletePublisher(natsWrapper.client).publish({
+        orderId: job.data.orderId,
+    });
 });
 
 export {
