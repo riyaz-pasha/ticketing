1929c654fba5f4c33fa4ede9445454b13bcc8f48
riyaz <riyazpasha880@gmail.com>
2021-04-13 22:33:31 +0530

    post user enter ticket details to the server and create a record in the db

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,16 +1,54 @@
+import { useState } from "react";
+import useRequest from "../../hooks/useRequest";
+
 const NewTicket = () => {
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+
+    const { doRequest, errors } = useRequest({
+        url: "/api/tickets",
+        method: "post",
+        body: {
+            title,
+            price
+        },
+        onSuccess: (ticket) => console.log(ticket)
+    })
+
+    const onBlur = () => {
+        const value = parseFloat(price);
+        if (isNaN(value)) return;
+        setPrice(value.toFixed(2));
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        doRequest();
+    }
+
     return (
         <div>
             <h1>Create a Ticket</h1>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-group">
                     <label>Title</label>
-                    <input className="form-control" />
+                    <input
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
                 </div>
                 <div className="form-group">
                     <label>Price</label>
-                    <input className="form-control" />
+                    <input
+                        className="form-control"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                        onBlur={onBlur}
+                    />
                 </div>
+                {errors}
                 <button className="btn btn-primary">Submit</button>
             </form>
         </div>
