aa173d251a79f8642c72d6d4f4a0f6051c8198db
riyaz <riyazpasha880@gmail.com>
2021-04-13 21:39:45 +0530

    pass current user to the every child component.. and pass currentUser and client to the rvery child getInitialProps method

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,11 @@
-import buildClient from "../api/buildClient"
-
 const LandingPage = ({ currentUser }) => {
     return currentUser ?
         <h1>You are signed in</h1> :
         <h1>You are NOT signed in</h1>
 }
 
-LandingPage.getInitialProps = async (context) => {
-    console.log("Landing Page")
-    const client = buildClient(context)
-    const { data } = await client.get("/api/users/currentuser");
-    return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+    return {};
 }
 
 export default LandingPage;
