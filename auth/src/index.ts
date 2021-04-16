2167b220fc5c52ca18f5bebea9e21a2ddc59e0d4
riyaz <riyazpasha880@gmail.com>
2021-04-17 00:41:40 +0530

    run docker build process of auth service - testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-    console.log("test docker build process through ci") // remove after testing this
+    console.log("test docker build process through ci.") // remove after testing this
 
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined!..");
